4360bacd346cdadf2c029e84029603ef4251aab7
krishna <devenger@192.168.1.6>
2022-01-31 22:07:56 +0530

    bug fix1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const format = require('date-format');
 
 const app = express()
 
-const PORT = 4000 || process.env.PORT
+const PORT =  process.env.PORT || 4000
 
 app.get('/', (req,res) => {
     res.json({
